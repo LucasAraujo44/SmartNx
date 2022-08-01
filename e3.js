97e08c10ceb7fc050f2ee0fa59456574dabd13f8
Lucas araujo <lucasaraujoo44.la@gmail.com>
2022-08-01 15:54:43 -0300

    Feat:     result('teste 1 de teste 2 string 3')

---
+++ e3.js
@@ -0,0 +1,8 @@
+function argumentos() {
+    var somaArgumentos = '';
+    [].slice.call(arguments, 0).forEach(function (result) {
+        somaArgumentos += ' '.concat(result);
+    })
+    console.log(somaArgumentos)
+}
+argumentos('Olá', 'Lucas', 'Araujo', '!')
