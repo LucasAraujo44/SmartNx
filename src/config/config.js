21dfc09b405186cbf81aab926f93640280b6d4bb
Lucas araujo <lucasaraujoo44.la@gmail.com>
2022-08-03 10:16:58 -0300

    Refactor: indentation of the file  structure

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
 const dotenv = require('dotenv')
-
 dotenv.config({
     path: process.env.NODE_ENV,
 })
--- src/infra/database/mongo/index.js
+++ src/infra/database/mongo/index.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 const config = require('../../../config/config')
 require('dotenv').config()
 
-class Database{
-    connect(){
+class Database {
+    connect() {
         return mongoose.connect
-        (process.env.DB_MONGO,
-        {
-            user: config.database.username,
-            pass: config.database.password
-        })
+            (process.env.DB_MONGO,
+                {
+                    user: config.database.username,
+                    pass: config.database.password
+                })
     }
 }
 module.exports = new Database().connect()
